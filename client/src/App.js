db780d728ff634add4b879575ce38220d1d5f8af
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2022-10-05 21:13:51 +0300

    Routes and toaster packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Toaster } from "react-hot-toast";
+import Log from './pages/Log';
+import Reg from './pages/Reg';
+
 function App() {
   return (
-    <div className="App">
-      <h2>Booking App</h2>
-    </div>
+    <BrowserRouter>
+      <Toaster position="top-center" reverseOrder={false} />
+      <Routes>
+        <Route path='/login' element={<Log />} />
+        <Route path='/register' element={<Reg />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
