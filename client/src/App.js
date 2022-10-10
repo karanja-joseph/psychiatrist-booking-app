888b59415bfa937b68367dcf9cf25e97c5656349
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2022-10-10 18:44:06 +0300

    import redux files and store

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useSelector } from 'react-redux'
 import { Toaster } from "react-hot-toast";
 import Log from './pages/Log';
 import Reg from './pages/Reg';
 import Home from './pages/Home';
+import SecuredRoute from './components/SecuredRoute'
+import PublicRoute from './components/publicRoute';
+
 
 function App() {
+
+  const { loading } = useSelector(state => state.alerts)
+
   return (
     <BrowserRouter>
+      {loading && <div className='spinner-parent'>
+        <div className="spinner-grow" role="status">
+
+        </div>
+      </div>}
       <Toaster position="top-center" reverseOrder={false} />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Log />} />
-        <Route path='/register' element={<Reg />} />
+        <Route path='/' element={<SecuredRoute><Home /></SecuredRoute>} />
+        <Route path='/login' element={<PublicRoute><Log /></PublicRoute>} />
+        <Route path='/register' element={<PublicRoute><Reg /></PublicRoute>} />
       </Routes>
     </BrowserRouter>
   );
