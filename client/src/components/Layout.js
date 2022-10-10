56030733da56a00d29bcdddf5ba25e2a8e7e811e
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2022-10-10 18:43:31 +0300

    main layout template @karaja-joseph

---
+++ client/src/components/Layout.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "../layout.css";
+
+function Layout(props) {
+    return (
+        <div className="main">
+            <div className="d-flex layout">
+                <div className="sidebar">
+
+                </div>
+
+                <div className="content">
+                    <div className="header">
+
+                    </div>
+
+                    <div className="body">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Layout;
