2dfb8623588ec4b3d359e64e9c95d559b7ebe941
joseph karanja <josephkaranja033@gmail.com>
2022-10-14 23:37:00 +0300

    Added more api's

--- server.js
+++ server.js
@@ -4,13 +4,18 @@ const app = express();
 
 require('dotenv').config()
 const dbSetup = require('./dbSetup')
-const patientRoute = require('./routes/patientRoute')
+const psychiatristRoute = require('./routes/psychiatristRoute')
+const userRoute = require('./routes/userRoute')
+const adminRoute = require('./routes/adminRoute')
+const path = require("path");
 
 const PORT = process.env.PORT || 5000;
 // console.log(process.env.DB_URL);
 
 app.use(cors())
 app.use(express.json())
-app.use('/api/patient', patientRoute)
+app.use('/api/user', userRoute)
+app.use('/api/psychiatrist', psychiatristRoute)
+app.use('/api/admin', adminRoute)
 
 app.listen(PORT, () => console.log(`Server runs on port ${PORT} !`))
