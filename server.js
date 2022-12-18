072251315b62ad2643123dbee3cba1387d72c82e
karanja-joseph <josephkaranja033@gmail.com>
2022-12-18 11:03:48 +0300

    env node

--- server.js
+++ server.js
@@ -20,11 +20,13 @@ app.use("/api/user", userRoute);
 app.use("/api/psychiatrist", psychiatristRoute);
 app.use("/api/admin", adminRoute);
 
-app.use("/", express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static("client/build"));
 
-app.get("*", (req, res) => {
-  res.sendFile(__dirname, "client/build/index.html");
-});
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server runs on port ${PORT} !`));
 
